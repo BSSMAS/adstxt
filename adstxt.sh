b0e289a354e5a33e4ea0a0cd8d391a11ed162ca7
BSSMAS <97737954+BSSMAS@users.noreply.github.com>
2023-10-31 11:29:38 +0100

    Update adstxt.sh
    mejoras de código y organización

--- adstxt.sh
+++ adstxt.sh
@@ -14,29 +14,33 @@ CFin="\033[0m\e[0m"
 arg=$#
 url="https://"$1"/ads.txt"
 
-ComGit=$(which git | awk '{print $2 $3}')
-ComCurl=$(which curl | awk '{print $2 $3}')
-ComJQ=$(which jq | awk '{print $2 $3}')
+#ComGit=$(which git | awk '{print $2 $3}')
+#ComCurl=$(which curl | awk '{print $2 $3}')
+#ComJQ=$(which jq | awk '{print $2 $3}')
 
+# EXISTE LA URL #################################
 estado=$(curl -s $url --head | grep 200 | awk '{print $2}' | head -1)
-Nregistro=$(curl -s $url | awk -F',' '!/^#/ {print $1 $2 $3 $4}' | wc | awk '{print $1}')
-NregistroFiltrada=$(curl -s $url | awk -F',' '!/^#/ {print $1 $2 $3 $4}' | grep "$2" | wc | awk '{print $1}')
+# NÚMERO DE REGISTROS ###########################
+Nregistro=$(curl -s $url | awk -F',' '!/^#/ {print tolower($1)}' | wc | awk '{print tolower($1)}')
+NregistroFiltrada=$(curl -s $url | awk -F',' '!/^#/ {print tolower($1)}' | grep "$2" | wc | awk '{print tolower($1)}')
+
 
 echo -e "\n${CVerde}[+]${CFin} ${CBlanco}La url: $1${CFin}"
 echo -e "-----------------------------------"
 
 if [[ "$estado" == "200" ]]
 then
-	if [ $arg >1 ]
+	if [ $arg -gt 1 ]
 	then
 		echo -e "${CTurquesa}[+]${CFin} ${CTurquesa}Filtro activo: \t $2 ${CFin}"
 		echo -e "${CTurquesa}[+]${CFin} ${CTurquesa}Nº de registros: \t $NregistroFiltrada ${CFin}"
 		echo -e "-----------------------------------"
-		curl -s $url | awk -F',' '!/^#/ {print $1 "\t" $2 "\t" $3 "\t" $4}' | grep "$2" | column -t -N Dominio:,ID-Publisher:,Relación:,ID-Plataforma: -s $'\t'
+		curl -s $url | awk -F',' '!/^#/ {print tolower($1 "\t" $2 "\t" $3 "\t" $4)}' | grep "$2" | column -t -N Dominio:,ID-Publisher:,Relación:,ID-Plataforma: -s $'\t'
 	else
 		echo -e "${CTurquesa}[+]${CFin} ${CTurquesa}Nº de registros: \t $Nregistro ${CFin}"
 		echo -e "-----------------------------------"
-		curl -s $url | awk -F',' '!/^#/ {print $1 "\t" $2 "\t" $3 "\t" $4 "\n----"}' | column -t -N Dominio:,ID-Publisher:,Relación:,ID-Plataforma: -s $'\t'
+		curl -s $url | awk -F',' '!/^#/ {print tolower($1 "\t" $2 "\t" $3 "\t" $4)}' | column -t -N Dominio:,ID-Publisher:,Relación:,ID-Plataforma: -s $'\t'
+
 	fi
 else
 	echo -e "${CRojo}[+] La url pasada no dispone de archivo ads.txt${CFin}\n"
