d07770185f5a1803e1005380670b6e1435a25030
BSSMAS <97737954+BSSMAS@users.noreply.github.com>
2023-11-02 13:00:14 +0100

    nuevos contadores
    SE ñade al 1º y final nuevos contadores, para saber cuantos reselles / direct hay

--- adstxt.sh
+++ adstxt.sh
@@ -20,9 +20,6 @@ url="https://"$1"/ads.txt"
 
 # EXISTE LA URL #################################
 estado=$(curl -s $url --head | grep 200 | awk '{print $2}' | head -1)
-# NÚMERO DE REGISTROS ###########################
-Nregistro=$(curl -s $url | awk -F',' '!/^#/ {print tolower($1)}' | wc | awk '{print tolower($1)}')
-NregistroFiltrada=$(curl -s $url | awk -F',' '!/^#/ {print tolower($1)}' | grep "$2" | wc | awk '{print tolower($1)}')
 
 
 echo -e "\n${CVerde}[+]${CFin} ${CBlanco}La url: $1${CFin}"
@@ -30,6 +27,13 @@ echo -e "-----------------------------------"
 
 if [[ "$estado" == "200" ]]
 then
+
+# NÚMERO DE REGISTROS ###########################
+Nregistro=$(curl -s $url | awk -F',' '!/^#/ {print tolower($1)}' | wc | awk '{print tolower($1)}')
+NregistroFiltrada=$(curl -s $url | awk -F',' '!/^#/ {print tolower($1)}' | grep "$2" | wc | awk '{print tolower($1)}')
+NregistroFilDirect=$(curl -s $url | awk -F',' '!/^#/ {print tolower($3)}' | grep "direct" | wc | awk '{print tolower($1)}')
+NregistroFilReseller=$(curl -s $url | awk -F',' '!/^#/ {print tolower($3)}' | grep "reseller" | wc | awk '{print tolower($1)}')
+
 	if [ $arg -gt 1 ]
 	then
 		echo -e "${CTurquesa}[+]${CFin} ${CTurquesa}Filtro activo: \t $2 ${CFin}"
@@ -39,9 +43,21 @@ then
 	else
 		echo -e "${CTurquesa}[+]${CFin} ${CTurquesa}Nº de registros: \t $Nregistro ${CFin}"
 		echo -e "-----------------------------------"
-		curl -s $url | awk -F',' '!/^#/ {print tolower($1 "\t" $2 "\t" $3 "\t" $4)}' | column -t -N Dominio:,ID-Publisher:,Relación:,ID-Plataforma: -s $'\t'
+		echo -e "${CTurquesa}[+]${CFin} ${CTurquesa}Nº de Direct: \t $NregistroFilDirect ${CFin}"
+		echo -e "-----------------------------------"
+		echo -e "${CTurquesa}[+]${CFin} ${CTurquesa}Nº de Reseller: \t $NregistroFilReseller ${CFin}"
+		echo -e "-----------------------------------"
+		curl -s $url | awk -F',' 'BEGIN{ print "Dominio:\tID-Publisher:\tRelación:\tID-Plataforma:"; } !/^#/ { print tolower($1 "\t" $2 "\t" $3 "\t" $4)}' | column -t -s $'\t'
+		echo -e "${CTurquesa}[+]${CFin} ${CTurquesa}Nº de registros: \t $Nregistro ${CFin}"
+		echo -e "-----------------------------------"
+		echo -e "${CTurquesa}[+]${CFin} ${CTurquesa}Nº de Direct: \t $NregistroFilDirect ${CFin}"
+		echo -e "-----------------------------------"
+		echo -e "${CTurquesa}[+]${CFin} ${CTurquesa}Nº de Reseller: \t $NregistroFilReseller ${CFin}"
+		echo -e "-----------------------------------"
 
 	fi
 else
 	echo -e "${CRojo}[+] La url pasada no dispone de archivo ads.txt${CFin}\n"
 fi
+
+
